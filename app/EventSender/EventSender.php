d800c05efad41bce0ef470650af8296aadf11578
user <user@Debian.myguest.virtualbox.org>
2025-01-12 21:52:26 +0500

    RabbitMQ

--- app/EventSender/EventSender.php
+++ app/EventSender/EventSender.php
@@ -1,12 +1,33 @@
 <?php
 namespace App\EventSender;
 use App\Telegram\TelegramApi;
+use App\Queue\Queue;
+use App\Queue\Queueable;
 
-class EventSender
+class EventSender implements Queueable
 {
-    private TelegramApi $telegram;
+    private string $receiver;
+    private string $message;
+
+    public function __construct(private TelegramApi $telegram, private Queue $queue)
+    {
+        
+    }
+
     public function sendMessage(string $receiver, string $message)
     {
-        echo date('d.m.y H:i') . " Я отправил сообщение $message получателю с id $receiver\n";
+        $this->toQueue($receiver, $message);
+    }
+
+    public function handle(): void
+    {
+        $this->telegram->sendMessage($this->receiver, $this->message);
+    }
+
+    public function toQueue(...$args): void
+    {
+        $this->receiver = $args[0];
+        $this->message = $args[1];
+        $this->queue->sendMessage(serialize($this));
     }
 }
